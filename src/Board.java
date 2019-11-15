9a1c3eac914b43126dcffd5cfe28269539fa397e
tomsloj <tomsloj@gmail.com>
2019-11-15 16:44:43 +0100

    add project

---
+++ src/Board.java
@@ -0,0 +1,52 @@
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+
+public class Board {
+    private int[][] table;
+    /*
+    0 - empty field
+    1 - snake
+    2 - point
+     */
+    ArrayList<Pair<Integer, Integer> >emptyPoints;
+    private int headX;
+    private int headY;
+    private int tailX;
+    private int tailY;
+    private int pointX;
+    private int pointY;
+
+    Board (int x, int y)
+    {
+        table = new int[x][y];
+        for( int i = 0; i < x; ++i )
+            for( int j = 0; j < y; ++j )
+            {
+                table[i][j] = 0;
+                emptyPoints.add(new Pair(i,j));
+            }
+
+        //setHead(x/2, y/2);
+
+
+    }
+
+    void setHead(int x, int y)
+    {
+        headX = x;
+        headY = y;
+    }
+
+    void setTail(int x, int y)
+    {
+        tailX = x;
+        tailY = y;
+    }
+
+    int getField(int x, int y)
+    {
+        return table[x][y];
+    }
+
+}
